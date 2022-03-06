2fcff301ecf323ff97e7826947c13e54dfba9544
Amol Bohora <007bohora@gmail.com>
2022-03-06 18:03:24 +0530

    Problem 40 Solved

--- 40.cpp
+++ 40.cpp
@@ -1,8 +1,9 @@
-// Problem : 39. Combination Sum
-// Link : https://leetcode.com/problems/combination-sum/
+// Problem : 40. Combination Sum II
+// Link : https://leetcode.com/problems/combination-sum-ii/
 
 #include <iostream>
 #include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -14,16 +15,19 @@ class Solution {
             ans.push_back(curr_combination);
             return;
         }
-        for (int i = currIndex; i < candidates.size(); i++) {
+        for (int i = currIndex; i < candidates.size(); ++i) {
+            if(i > currIndex && candidates[i] == candidates[i-1])
+                continue;
             curr_combination.push_back(candidates[i]);
             currSum += candidates[i];
-            combination(candidates, target, curr_combination, currSum, i, ans);
+            combination(candidates, target, curr_combination, currSum, i + 1, ans);
             curr_combination.pop_back();
             currSum -= candidates[i];
         }
     }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort( candidates.begin(), candidates.end() );
         vector<vector<int>> ans;
         vector<int> curr_combination;
         combination(candidates, target, curr_combination, 0, 0, ans);
@@ -33,8 +37,8 @@ public:
 
 int main() {
     Solution ob;
-    vector<int> candidates{2,3,6,7};
-    vector<vector<int>> solution = ob.combinationSum(candidates, 7);
+    vector<int> candidates{10,1,2,7,6,1,5};
+    vector<vector<int>> solution = ob.combinationSum(candidates, 8);
     for(const vector<int>& i : solution){
         for(int j : i)
             cout << j << ",";
--- Readme.md
+++ Readme.md
@@ -8,3 +8,4 @@
 |   84   | [Largest Rectangle in Histogram](/84.cpp) [`Code`]                          | <span style="color:green">Accepted</span> | 175 ms | 77.3 MB |
 |   38   | [Count and Say](/38.cpp) [`Code`]                                           | <span style="color:green">Accepted</span> | 8 ms   | 6.7 MB  |
 |   39   | [Combination Sum](/39.cpp) [`Code`]                                         | <span style="color:green">Accepted</span> | 58 ms  | 18.8 MB |
+|   40   | [Combination Sum II](/40.cpp) [`Code`]                                      | <span style="color:green">Accepted</span> | 32 ms  | 16.3 MB |
